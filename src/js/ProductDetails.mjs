092bada9a41de72be8318438dc911cc267a86039
Greice Moreira <greice.f.d.a@hotmail.com>
2025-04-04 14:12:14 -0400

    image

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -56,7 +56,7 @@ export default class ProductDetails {
   
             <img
              class="divider"
-             src="${product.Image}"
+             src="${product.Images.PrimaryLarge}"
              alt="${product.NameWithoutBrand}"
             />
            <p class="product-card__price">$${product.FinalPrice}</p>
